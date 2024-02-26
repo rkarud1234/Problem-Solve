cfddc46bea93c2730d5f58384656cafa490d47b9
10154533 <kapkyeong.kim@kt.com>
2024-02-26 21:31:32 +0900

    BOJ25501

---
+++ BOJ/BOJ25501.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.*;
+
+public class BOJ25501 {
+
+    private static int cnt;
+
+    public static int recursion(String s, int l, int r) {
+        cnt++;
+        if (l >= r) return 1;
+        else if (s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l + 1, r - 1);
+    }
+d
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            cnt = 0;
+            String S = br.readLine();
+            System.out.println(recursion(S, 0, S.length() - 1) + " " + cnt);
+        }
+    }
+}
